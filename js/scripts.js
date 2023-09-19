e5055b8b5eb262d4948ccddaea6e821f525db241
Paris Vigay <parisvigay@pariss-mbp.home>
2023-09-19 20:00:51 +0100

    day2

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+// state variables
+// let mainBoard; // 8 rows of holes plus one row of solution holes
+// let winner; // Null = didn't work out solution; Winner = did work out solution
+// let solutionHoles; // Will reveal sedret colours once player has had 8 tries and/or won 
+
+let colors  = ['#DC143C', '#89CFF0', '#FFEA00', '#FAA0A0', '#0BDA51', 'darkorchid'];
+
+let guessNumber = 0;
+
+let guessPosition = 0;
+
+//Generates a random combo of colors
+function generateRandomColors(colors) {
+   let a = colors[Math.floor(Math.random() * colors.length)];
+   let b = colors[Math.floor(Math.random() * colors.length)];
+   let c = colors[Math.floor(Math.random() * colors.length)];
+   let d = colors[Math.floor(Math.random() * colors.length)];
+    let solution = [];
+    solution.push(a);
+    solution.push(b);
+    solution.push(c);
+    solution.push(d);
+    console.log(solution);
+    document.getElementById("solutionHole0").style.backgroundColor = `${solution[0]}`;
+    document.getElementById("solutionHole1").style.backgroundColor = `${solution[1]}`;
+    document.getElementById("solutionHole2").style.backgroundColor = `${solution[2]}`;
+    document.getElementById("solutionHole3").style.backgroundColor = `${solution[3]}`;
+}
+generateRandomColors(colors);
+
+const playerGuesses = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]];
+
+function chosenColor(event) {
+    document.getElementById("hole0").style.backgroundColor = (event.target.value);
+    playerGuesses[0].splice(0, 1, event.target.value)
+    console.log(playerGuesses[0]);
+}
+
+
+//Guess array of 8 arrays - query selector all
+//for each - click event - next available 0 (1 is already assigned)
+
+// guess position, change index in playerGuesses array to chosen colour. Update the class of the individual hole with selected colour.
+// update player guesses variable to a colour string
+// if guess position = 3, increase guess number by 1
+// guess number effects which purple array. guess position is the individual value
